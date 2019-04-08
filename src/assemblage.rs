f842baaaf0e9a962365018ed3787fe9acb784ad6
Harley Swick <hdswick@gmail.com>
2019-04-08 02:01:01 -0500

    Refactored heavily

--- src/assemblage.rs
+++ src/assemblage.rs
@@ -1,141 +1,51 @@
 use std::fmt;
 
-type Intensity = f64;
-
-#[derive(Debug)]
-pub struct ExtrinsicRelation {
-    source: usize,
-    target: usize,
-    intensity: Intensity,
-    label: Option<String>
-}
-
-impl fmt::Display for ExtrinsicRelation {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:#?}", self)
-    }
-}
-
-#[derive(Debug)]
-pub struct Value {
-    index: usize,
-    intensity: Intensity
-}
-
-impl fmt::Display for Value {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:#?}", self)
-    }
-}
-
-//Trajectory is an ordered sequence of coordinates in possibility space
-//Possibility space is the cross product of the asssemblage's relations
-type Coordinate = Vec<Value>;
-type Trajectory = Vec<Coordinate>;
-
-//Components and relations should be thought of as a set. All components and relations should be unique.
-//For memory purposes we will use vectors, so we can list relations as the indices of the vector of components.
-#[derive(Debug, Default)]
+#[derive(Debug, Clone)]
 pub struct Assemblage {
-    components: Vec<Box<Assemblage>>,
-    relations: Option<Vec<ExtrinsicRelation>>,
-    trajectory: Option<Trajectory>
-}
-
-pub fn initial_relations() -> Option<Vec<ExtrinsicRelation>> {
-    let r = ExtrinsicRelation {
-        source: 0, target: 0, intensity: 0.0, label: Some("self".to_string())
-    };
-    Some(vec![r])
-}
-
-pub fn initial_trajectory() -> Option<Trajectory> {
-    let v = Value {
-        index: 0, intensity: 0.0
-    };
-    Some(vec![vec![v]])
+    name: String,
+    relation: Option<(Box<Assemblage>, Box<Assemblage>)>,
+    intensity: f64,
+    log: Vec<f64>
 }
 
 impl Assemblage {
 
     pub fn new() -> Assemblage {
-        //Look up Deleuze empty set (Lacan & Deleuze)
-        //There is always one element in the empty set, the empty set itself.
-        //This is how we can have a self relation
-        let a = Assemblage {
-            components: Vec::new(),
-            .. Default::default()
-        };
-        
         Assemblage {
-            components: vec![Box::new(a)],
-            relations: initial_relations(),
-            trajectory: initial_trajectory()
+            name: "Null".to_string(),
+            intensity: 0.0,
+            log: vec![0.0],
+            relation: None
         }
     }
 
-    pub fn components(mut self, components: Vec<Box<Assemblage>>) -> Self {
-        self.components = components;
+    pub fn name(mut self, name: &str) -> Self {
+        self.name = name.to_string();
         self
     }
 
-    pub fn relations(mut self, relations: Vec<ExtrinsicRelation>) -> Self {
-        self.relations = Some(relations);
+    pub fn intensity(mut self, intensity: f64) -> Self {
+        self.intensity = intensity;
+        self.log = vec![intensity];
         self
     }
 
-    pub fn trajectory(mut self, trajectory: Trajectory) -> Self {
-        self.trajectory = Some(trajectory);
+    pub fn relation(mut self, relation: (Assemblage, Assemblage)) -> Self {
+        let (a, b) = relation;
+        self.relation = Some((Box::new(a), Box::new(b)));
         self
     }
 
-    pub fn territorialization(self) -> f64 {
-        0.0
-    }
-
-    pub fn codification(self) -> f64 {
-        0.0
-    }
-
-    pub fn push_component(mut self, a: Assemblage) -> Self {
-        self.components.push(Box::new(a));
-        self
-    }
-
-    pub fn push_relation(mut self, r: ExtrinsicRelation) -> Self {
-        self.relations = match self.relations {
-            Some(mut rs) => {
-                rs.push(r);
-                Some(rs)
-            }, None => {
-                None
-            }
-        };
-        self
-    }
-
-    pub fn push_value(mut self, index: usize, v: Value) -> Self {
-        self.trajectory = match self.trajectory {
-            Some(mut t) => {
-                t[index].push(v);
-                Some(t)
-            }, None => {
-                None
-            }
-        };
-        self
-    }
-
-    pub fn push_coordinate(mut self, c: Coordinate) -> Self {
-        self.trajectory = match self.trajectory {
-            Some(mut t) => {
-                t.push(c);
-                Some(t)
-            }, None => {
-                None
-            }
-        };
-        self
+    // Difference in degree
+    pub fn update(mut self, intensity: f64) -> Self {
+        match self.relation {
+            Some(_) => {
+                self.intensity = intensity;
+                self.log.push(intensity);
+                self
+            },
+            None => { self }
+        }
     }
 
 }
@@ -146,38 +56,14 @@ impl fmt::Display for Assemblage {
     }
 }
 
-pub fn add(a: Assemblage, b: Assemblage) -> Assemblage {    
+pub fn null() -> Assemblage {
     Assemblage::new()
-        .push_component(a)
-        .push_component(b)
-        .push_relation(ExtrinsicRelation {
-            source: 0,
-            target: 1,
-            intensity: 0.0,
-            label: None
-        })
-        .push_relation(ExtrinsicRelation {
-            source: 0,
-            target: 2,
-            intensity: 0.0,
-            label: None
-        })
-        .push_relation(ExtrinsicRelation {
-            source: 1,
-            target: 2,
-            intensity: 0.0,
-            label: None
-        })
-        .push_value(0, Value {
-            index: 1,
-            intensity: 0.0
-        })
-        .push_value(0, Value {
-            index: 2,
-            intensity: 0.0
-        })
-        .push_value(0, Value {
-            index: 3,
-            intensity: 0.0
-        })
+}
+
+// Define the relation between two assemblages.
+pub fn and(a: Assemblage, b: Assemblage, name: &str, intensity: f64) -> Assemblage {
+    Assemblage::new()
+        .name(name)
+        .intensity(intensity)
+        .relation((a, b))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
 mod assemblage;
-use assemblage::Assemblage;
 
 fn main() {
-    let a = Assemblage::new();
-    let b = Assemblage::new();
-    let c = assemblage::add(a, b);
-
+    
+    let a = assemblage::null();
+    let b = assemblage::null();
+    let c = assemblage::and(a, b, "Primitive Assemblage", 42.0);
     println!("{}", c);
+
+    // Define Cartesian space with assemblages
+    let origin = assemblage::null();
+    let x = assemblage::and(origin.clone(), origin.clone(), "X", 2.0);
+    let y = assemblage::and(origin.clone(), origin.clone(), "Y", 4.0);
+    let point2 = assemblage::and(x, y, "2D point", 0.0);
+    println!("{}", point2);
+
+    let z = assemblage::and(origin.clone(), origin.clone(), "Z", 42.0);
+    let point3 = assemblage::and(point2, z, "3D Point", 0.0);
+    println!("{}", point3);
+
+    // Builder syntax
+    let allison = assemblage::null().name("Allison");
+    let harley = assemblage::null().name("Harley");
+    let love = assemblage::and(harley, allison, "Love of Harley and Allison", 1.0);
+    println!("{}", love);
+   
 }
